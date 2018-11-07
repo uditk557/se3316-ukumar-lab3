85a7587ae7b277d187d1c50fb3503d5ecc18f4c0
uditk557 <35313576+uditk557@users.noreply.github.com>
2018-11-07 16:43:15 -0500

    Connect to mongodb

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient; var db; // Mongodb variables
 
-MongoClient.connect('mongodb://<ukumar>:<Phone#6478084879>@ds155203.mlab.com:55203/onlinestore' , (err, client) => {
+MongoClient.connect('mongodb://ukumar:Phone6478084879@ds155203.mlab.com:55203/onlinestore' , (err, client) => {
     if (err) return console.log(err);
     db = client.db('onlinestore');
 
-    app.listen(557, () => {
-        console.log('test one complete');
-    });
+    app.listen(557);
+    console.log('Test 1');
 
+    app.get('/', (req, res) => {
+        res.send('Test 2');
+    });
 
 
 
