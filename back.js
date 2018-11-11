0fa531a5f298b85ea8d4e2a5fd7e6851a1e98873
uditk557 <35313576+uditk557@users.noreply.github.com>
2018-11-11 14:09:09 -0500

    Made changes to back end server

--- back.js
+++ back.js
@@ -4,8 +4,12 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 var mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 var items = require ('/Users/ukumar/Documents/GitHub/se3316-ukumar-lab3/app/models' + '/items.js');
-
+var db;
+MongoClient.connect('mongodb://ukumar:Phone6478084879@ds155203.mlab.com:55203/onlinestore', (err,client)=>{
+    db = client.db('onlinestore');
+}) ;
 mongoose.connect('mongodb://ukumar:Phone6478084879@ds155203.mlab.com:55203/onlinestore');
 
 var port = 800;
@@ -37,8 +41,10 @@ app.put("/items/update", function(req,res){
     },{$set: {quantity: req.body.title}});
     });
 
-    app.delete("/item/delete", function(req,res) {
+    app.get("/item/delete", function(req,res) {
+        
         items.findOneAndRemove({
+           
             item: req.body.item
         }
         ,function(err, items) {
@@ -46,12 +52,23 @@ app.put("/items/update", function(req,res){
                 res.send("Error Deleting");
             } else {
                 console.log("deleted");
+               
             }
         })
     });
-    app.get("/items/show" , function(req,res){
+    app.get('/items/show', function(req,res) {
+       var cursor = db.collection('items').find().toArray(function(err, results) {
+        if (err) {
+
+        }else {
+            console.log("results");
+            res.send(results);
+        }
+       });
+    });
+    /*app.get("/items/show" , function(req,res){
         console.log("show");
     });
     app.post("/items/show", (req,res) =>{
         res.json(items);
-    });
+    });*/
